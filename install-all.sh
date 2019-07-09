e3df2373972e51a89663fc8b6d7c59b09a4d2214
Konstantin Gusev <guvkon@gmail.com>
2019-07-09 23:55:45 +0700

    Fix install-all.sh script

--- install-all.sh
+++ install-all.sh
@@ -6,17 +6,17 @@ CONFIGS_DIR="`pwd`"
 USER="`whoami`"
 
 move_and_link() {
-    if [ -f "~/$1" ] || [ -d "~/$1" ]; then
-        mv "~/$1" "~/$1.backup"
+    if [ -f "$HOME/$1" ] || [ -d "$HOME/$1" ]; then
+        mv "$HOME/$1" "$HOME/$1.backup"
     fi
-    ln -s "$CONFIGS_DIR/$1" "~/$1"
+    ln -s "$CONFIGS_DIR/$1" "$HOME/$1"
 }
 
 move_and_link .zshrc
 move_and_link .vimrc
 move_and_link .shell_aliases
 move_and_link .gitconfig
-mkdir -p "~/.vim/pack/$USER"
-ln -s "$CONFIGS_DIR/vim-packs" "~/.vim/pack/$USER/start"
+mkdir -p "$HOME/.vim/pack/$USER"
+ln -s "$CONFIGS_DIR/vim-packs" "$HOME/.vim/pack/$USER/start"
 git submodule init
 git submodule update
